0a4830c6b0a1207796a9780034cc766835ca18e4
vasutomar <vasutomar26@gmail.com>
2018-10-07 11:43:02 +0530

    "Resolved camera permission denial error."

--- app/src/main/java/vasu/lenovo/obsidianandroidapp/DecodeActivity.kt
+++ app/src/main/java/vasu/lenovo/obsidianandroidapp/DecodeActivity.kt
@@ -1,8 +1,12 @@
 package vasu.lenovo.obsidianandroidapp
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.ImageView
 
 class DecodeActivity : AppCompatActivity() {
@@ -18,5 +22,29 @@ class DecodeActivity : AppCompatActivity() {
         if (intent.resolveActivity(packageManager) != null) {
             startActivityForResult(intent, REQUEST_SELECT_IMAGE_IN_ALBUM)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            101 -> {
+                if (data != null) {
+                    val uri = data.data
+                    displaySelectedImage(getBitmapFromUri(uri))
+                }
+            }
+            else -> super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+    private fun getBitmapFromUri(uri: Uri): Bitmap {
+        val parcelFileDescriptor = contentResolver.openFileDescriptor(uri, "r")
+        val fileDescriptor = parcelFileDescriptor?.fileDescriptor
+        val image = BitmapFactory.decodeFileDescriptor(fileDescriptor)
+        parcelFileDescriptor.close()
+        return image
+    }
+
+    private fun displaySelectedImage(imageBitmap: Bitmap) {
+        val iv_selected_image = findViewById<ImageView>(R.id.DecImage)
+        iv_selected_image.setImageBitmap(imageBitmap)
     }
 }
--- app/src/main/java/vasu/lenovo/obsidianandroidapp/SendActivity.kt
+++ app/src/main/java/vasu/lenovo/obsidianandroidapp/SendActivity.kt
@@ -1,15 +1,22 @@
 package vasu.lenovo.obsidianandroidapp
 
+import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.view.View
 import android.widget.ImageView
 
 class SendActivity : AppCompatActivity() {
+
+    private var REQUEST_SELECT_IMAGE_IN_ALBUM = 0
+
+    private val PERMISSIONS_REQUIRED = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,12 +26,41 @@ class SendActivity : AppCompatActivity() {
         val mainicon = findViewById<ImageView>(R.id.MainIcon)
         mainicon.setImageResource(R.drawable.icon)
 
+        askRequiredPermissions()
+
+    }
+
+    private fun arePermissionGranted(): Boolean {
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            return false
+        }
+        return true
+    }
+
+    private fun askRequiredPermissions() {
+        if (!arePermissionGranted()) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS_REQUIRED, 101)
+        }
     }
 
     fun CaptureCamera(view: View) {
-
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, 1)
+            }
+        }
     }
     fun ChoosePicture(view: View) {
 
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivityForResult(intent, REQUEST_SELECT_IMAGE_IN_ALBUM)
+        }
     }
 }
