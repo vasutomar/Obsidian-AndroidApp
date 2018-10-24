aed8a4d9d2b885a579a0a6fb1d04bb989ff09fcd
vasutomar <vasutomar26@gmail.com>
2018-10-24 13:00:58 +0530

    Made UI minimalistic, added basic socket.io code

--- app/src/main/java/vasu/lenovo/obsidianandroidapp/MainActivity.kt
+++ app/src/main/java/vasu/lenovo/obsidianandroidapp/MainActivity.kt
@@ -1,31 +1,61 @@
 package vasu.lenovo.obsidianandroidapp
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.view.View
-import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    private val PERMISSIONS_REQUIRED = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA,Manifest.permission.INTERNET)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        askRequiredPermissions()
     }
 
-    fun StartSend(view : View) {
-        val intent = Intent(this,SendActivity::class.java)
-        startActivity(intent)
+    private fun arePermissionGranted(): Boolean {
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            return false
+        }
+        return true
     }
 
-    fun StartDecode(view : View) {
-        val intent = Intent(this,DecodeActivity::class.java)
-        startActivity(intent)
+    private fun askRequiredPermissions() {
+        if (!arePermissionGranted()) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS_REQUIRED, 101)
+        }
     }
 
     fun StartAbout(view : View) {
         val intent = Intent(this,AboutApp::class.java)
+        startActivity(intent)
+    }
+    fun CaptureCamera(view : View) {
+        val intent = Intent(this,SenderActivity()::class.java)
+        intent.putExtra("Code",1)
+        startActivity(intent)
+    }
+    fun ChoosePicture(view : View) {
+        val intent = Intent(this,SenderActivity()::class.java)
+        intent.putExtra("Code",2)
+        startActivity(intent)
+    }
+    fun DecodeImage(view : View) {
+        val intent = Intent(this,DecodeActivity::class.java)
         startActivity(intent)
     }
 }
--- app/src/main/java/vasu/lenovo/obsidianandroidapp/SenderActivity.kt
+++ app/src/main/java/vasu/lenovo/obsidianandroidapp/SenderActivity.kt
@@ -1,19 +1,23 @@
 package vasu.lenovo.obsidianandroidapp
-
-import android.Manifest
 import android.content.Intent
 import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
+import android.view.View
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import com.squareup.picasso.Picasso
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
 
 class SenderActivity() : AppCompatActivity() {
 
+    private val mSocket = IO.socket("http://chat.socket.io")
     private var REQUEST_SELECT_IMAGE_IN_ALBUM = 0
     private var REQUEST_CAPTURE_IMAGE = 1
+    private val message = String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +30,7 @@ class SenderActivity() : AppCompatActivity() {
             Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
                 //Log.e("I am","Capturing")
                 takePictureIntent.resolveActivity(packageManager)?.also {
-                    startActivityForResult(takePictureIntent, 1)
+                    startActivityForResult(takePictureIntent, REQUEST_CAPTURE_IMAGE)
                 }
             }
         }
@@ -36,7 +40,7 @@ class SenderActivity() : AppCompatActivity() {
             val intent = Intent(Intent.ACTION_GET_CONTENT)
             intent.type = "image/*"
             if (intent.resolveActivity(packageManager) != null) {
-                startActivityForResult(intent, 0)
+                startActivityForResult(intent, REQUEST_SELECT_IMAGE_IN_ALBUM)
             }
         }
     }
@@ -52,5 +56,12 @@ class SenderActivity() : AppCompatActivity() {
             val Img = findViewById<ImageView>(R.id.SendingImage)
             Img.setImageBitmap(imageBitmap)
         }
+    }
+    fun SendResources(view: View) {
+        print("here")
+        val user_message = findViewById(R.id.UserMessage) as EditText
+        Toast.makeText(this, user_message.text, Toast.LENGTH_SHORT).show()
+        mSocket.connect()
+        mSocket.emit("new message",user_message.text)
     }
 }
